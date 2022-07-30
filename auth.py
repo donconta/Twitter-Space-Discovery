acd6cc4ea275a8fa85d035c3398e022abbaf84ed
Temitope Omoniyi <57876143+donconta@users.noreply.github.com>
2022-07-30 14:39:01 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,14 +1,14 @@
 
 #Authentication Tokens
-access_token = "56163769-bAoCgZQ3HXEtaNawyhLolczjFyi9mpInsfRsglwyx"
-access_token_secret = "vR8I7IwzAUDqLQfJPFMbpOtSbOaENmg4OorvJ4A0CukfC"
+access_token = "Your Credential Here"
+access_token_secret = "Your Credential Here"
 
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAOPFVgEAAAAA6LCGNp5nBsfcqvld%2FJ%2FHgZhF7RM%3Dw3TD9TXthlO3lKGdHikKsZC84EdnspmqVLEUEAe6GhnNyW6LQe"
+bearer_token = "Your Credential Here"
 
 #consumer keys
-api_key = "6YkD2N1TcHC0eCQFLjifLHsCh"
-api_key_secret = "YsnVrFmJ2Lo5gnCN13MaUfETBuJptr8pmEIGzErSEQbh5DR2CZ"
+api_key = "Your Credential Here"
+api_key_secret = "Your Credential Here"
 
 #OAuth 2.0 Client ID and Client Secret
-client_ID = "RDdvcmI5Z3BmLWoyOXp2LTA4U2Q6MTpjaQ"
-client_secret = "qm_3F6FJD33WRmCUroG33OpUxI4cBEoZtQA3OpftMV5fsOx7x-"
+client_ID = "Your Credential Here"
+client_secret = "Your Credential Here"
